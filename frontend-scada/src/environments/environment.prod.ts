d243aeb07838373d97be66c414eaf335c31fe9d2
Kevin Abarca Gracia <104174251+kabarcagracia@users.noreply.github.com>
2022-06-02 00:48:01 -0400

    Add files via upload

---
+++ frontend-scada/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+/*export const environment = {
+  production: true,
+  serverSocket: 'https://mqtt-gcp-socket.herokuapp.com',
+  url: 'https://backendscada.herokuapp.com'
+};*/
+export const environment = {
+  production: true,
+  serverSocket: 'http://3.84.101.202:3001',
+  url: 'http://3.84.101.202:3000'
+};
